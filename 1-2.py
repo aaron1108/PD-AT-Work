8ac1e68946f6f48c7c0b7bc39a9e4cf8a62a69c8
aaron1108 <wtfcaiii@gmail.com>
2023-05-24 23:26:14 +0800

    refactor:1-2

--- 1-2.py
+++ 1-2.py
@@ -1,24 +1,11 @@
-import sys
 import json
-from month_search import month_search
+import month_search
 
 if __name__ == '__main__':
 
     with open('dict_json.json', encoding='utf-8') as jsonfile:
         month = json.load(jsonfile)
 
-    for num in range(1, 4):
+    result = month_search.month_filter()
 
-        char = input(f"請輸入第{num}個英文\n").lower()
-
-        result = month_search(month, char, num).char_search()
-
-        if len(result) == 1:
-
-            print(list(result.values())[0])
-
-            sys.exit()
-        
-        else:
-            month = result
-        
+    result.search_condtion(month)
--- month_search.py
+++ month_search.py
@@ -1,26 +1,40 @@
 import sys
-class month_search:
 
-    def __init__(self, month, char, num):
-        self.month = month
-        self.char = char
-        self.num = num
+class month_filter:
 
-    def char_search(self):
-        
+    @staticmethod
+    def search_process(month, char, num):
+
         my_dict = {}
 
-        if not self.char.isalpha():
-          
+        if not char.isalpha():
+
             print('請輸入英文')
             sys.exit()
+        
+        for key, value in month.items():
 
-        for key, value in self.month.items():
-
-            if self.char == format(key)[self.num - 1:self.num]:
+            if char == format(key)[num - 1:num]:
 
                 my_dict[format(key)] = format(value)
-    
+
         return my_dict
 
-        
+    def search_condtion(self, month):
+
+        for num in range(1, 4):
+
+            char = input(f"請輸入第{num}個英文\n").lower()
+
+            result = self.search_process(month, char, num)
+
+            if len(result) == 1:
+
+                print(list(result.values())[0])
+
+                sys.exit()
+
+            else:
+                month = result
+
+        print('輸入英文沒有符合的月份')
