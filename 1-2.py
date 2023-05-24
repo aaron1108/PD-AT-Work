f75fbd63330bb9b4de2c4ba925e35e117f287aac
aaron1108 <wtfcaiii@gmail.com>
2023-05-24 23:33:45 +0800

    rewrite:1-2 import

--- 1-2.py
+++ 1-2.py
@@ -1,11 +1,11 @@
 import json
-import month_search
+from month_search import month_filter
 
 if __name__ == '__main__':
 
     with open('dict_json.json', encoding='utf-8') as jsonfile:
         month = json.load(jsonfile)
 
-    result = month_search.month_filter()
+    result = month_filter()
 
     result.search_condtion(month)
