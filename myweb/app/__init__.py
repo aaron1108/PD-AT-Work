ee926d36be9facdb6fa4e65b58de18a59a7c9a41
aaron1108 <wtfcaiii@gmail.com>
2023-06-16 01:28:46 +0800

    feat:flask_set_done

--- myweb/app/__init__.py
+++ myweb/app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
-from app.route import index, other
+from app.route import index, other, get_wagerdiv, test_start
 
 
 def create_app():
 
     app = Flask(__name__)
 
-    app.add_url_rule('/index', 'index', index)
+    app.add_url_rule('/', 'index', index)
 
     app.add_url_rule('/other', 'other', other)
+
+    app.add_url_rule('/get_wagerdiv', None, get_wagerdiv)
+
+    app.add_url_rule('/start_test', None, test_start)
 
     return app
--- myweb/app/route.py
+++ myweb/app/route.py
@@ -1,4 +1,6 @@
 from flask import render_template
+from flask import request
+from urllib.parse import unquote
 
 
 def index():
@@ -6,4 +8,37 @@ def index():
 
 
 def other():
-    return render_template('other.html')
+    game_list = ['BB視訊', 'AG視訊', 'BB捕魚達人']
+    return render_template('other.html', game_list=game_list)
+
+
+def get_wagerdiv():
+
+    game = request.args.get('game')  # 獲取選擇的遊戲
+    decoded_game = unquote(game)
+    if (decoded_game == 'BB視訊'):
+
+        params = ['日期', '局號', '注單編號']
+
+    html = ''
+    # 根據選擇的遊戲生成對應的 HTML
+    for item in params:
+        html += '<div class="form-group form-row" id="wagerdiv">'
+        html += '<label for="length" class="col-md-4 col-form-label">' + item + '</label>'
+        html += '<div class="col-md-8">'
+        html += '<input type="text" name="' + item + \
+            '" class="form-control" id="length" min="4" max="16" value="' + item + '">'
+        html += '</div></div>'
+
+    return html
+
+
+def test_start():
+
+    form_data = request.form
+
+    for key, value in form_data:
+
+        print(f'key:{key},value:{value}')
+
+    return "Success"
